b8d09db2108dff6ac7439846f7881e1db7c6b21a
monsad <monikaksadlok@gmail.com>
2023-04-03 11:32:58 +0200

    Delete django-aws directory

--- django-aws/celery.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from celery import Celery
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_aws.settings")
-app = Celery("django_aws")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
--- django-aws/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django_aws.tasks import web_task
-
-
-def create_web_task(request):
-    web_task.delay()
-    return HttpResponse("Task added")

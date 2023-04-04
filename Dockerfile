2b902e5b770f1cf916756f18ef17186c5d9dcfeb
Monika Sadlok <>
2023-04-04 20:06:10 +0200

    add postgres, envirion and static files

--- Dockerfile
+++ Dockerfile
@@ -16,3 +16,5 @@ RUN pip install --no-cache-dir -r /requirements.txt
 
 WORKDIR /app
 COPY . /app
+
+RUN ./manage.py collectstatic --noinput
--- settings.py
+++ settings.py
@@ -1,3 +1,28 @@
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+env_path = BASE_DIR / ".env"
+if env_path.is_file():
+    environ.Env.read_env(env_file=str(env_path))
+
+
+DATABASES = {
+    'default': env.db(default="postgresql://postgres:postgres@127.0.0.1:5433/django_aws")
+}
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+]
+
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
